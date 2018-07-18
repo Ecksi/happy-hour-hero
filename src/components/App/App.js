a1bdf68fd372ec9d9b1ec19e26c7a0a84c4c3241
Tyler McNamara <mcnamara14@gmail.com>
2018-07-17 21:05:13 -0600

    Add logo and search input to homepage

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
-import './App.scss';
+import './App.css';
+import homeLogo from './images/home-logo.png';
 
 class App extends Component {
+  constructor (props) {
+    super(props);
+
+  }
+
+  handleSubmit = () => {
+
+  }
+
   render() {
     return (
       <div className="App">
-        REACT YO FACE!
+        <section className="homeContainer">
+          <img src={homeLogo} className="homeLogo" alt="Happy Hour Hero Logo" />
+          <h2>Find your happy hour:</h2>
+          <form className="homeSearchForm" onSubmit={this.handleSubmit}>
+            <input 
+              className="homeSearchInput" 
+              placeholder="Enter a restaurant or location" 
+            />
+            <input className="homeSearchSubmit" type="submit" value="Submit" />
+          </form>
+        </section>
       </div>
     );
   }
