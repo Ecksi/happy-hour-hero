d399bc4a3d361dbd6de1f6e6db04644a169cbaf0
Ecksi <jimmy.dechant@gmail.com>
2018-07-18 17:11:41 -0600

    Add initial migration

---
+++ db/migrations/20180718164711_initial.js
@@ -0,0 +1,50 @@
+exports.up = function (knex, Promise) {
+  return Promise.all([
+    knex.schema.createTable('restaurants', table => {
+      table.increments('id').primary();
+      table.string('name');
+      table.string('address');
+      table.integer('phone');
+      table.string('website');
+      table.string('city');
+      table.string('state');
+      table.integer('zip_code');
+      table.string('restaurant_image');
+      table.timestamps(true, true);
+    }),
+
+    knex.schema.createTable('happy_hours', table => {
+      table.increments('id').primary();
+      table.string('day(s)');
+      table.string('start_time');
+      table.string('end_time');
+      table.integer('restaurant_id').references('restaurants.id');
+      table.integer('food_specials_id').references('food_specials.id');
+      table.integer('drink_specials_id').references('drink_specials.id');
+      table.timestamps(true, true);
+    }),
+
+    knex.schema.createTable('drink_specials', table => {
+      table.increments('id').primary();
+      table.string('name');
+      table.boolean('best_deal').notNullable().defaultTo(false);
+      table.timestamps(true, true);
+    }),
+
+    knex.schema.createTable('food_specials', table => {
+      table.increments('id').primary();
+      table.string('name');
+      table.boolean('best_deal').notNullable().defaultTo(false);
+      table.timestamps(true, true);
+    })
+  ]);
+};
+
+exports.down = function (knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable('food_specials'),
+    knex.schema.dropTable('drink_specials'),
+    knex.schema.dropTable('happy_hours'),
+    knex.schema.dropTable('restaurants')
+  ]);
+};
