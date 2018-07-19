d80ce1a445fd0e5bc4456899db2498744e354a87
Tyler McNamara <mcnamara14@gmail.com>
2018-07-19 17:24:59 -0600

    Complete location input setup

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,15 +9,15 @@ class Header extends Component {
   }
 
   render() {
-    const { city, state, zip, address, longitude, latitude } = this.props.location;
+    const { address } = this.props.location;
 
     return (
       <header>
         <section className="headerContainer">
           <i className="fas fa-bars"></i>
           <div className="headerLocation">
-            { this.props.location.city ? <p>{ city }, { state } </p> : <p>{ address }</p> }
-            <i class="fas fa-caret-down"></i>
+            <p>{ address }</p>
+            <i className="fas fa-caret-down"></i>
           </div>
         </section>
       </header>
