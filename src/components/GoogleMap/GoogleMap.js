e23289ffc956513441e9c76aff24fc8739c72c9a
Tyler McNamara <mcnamara14@gmail.com>
2018-07-18 21:38:48 -0600

    Add simple markers for user and other locations

--- src/components/GoogleMap/GoogleMap.js
+++ src/components/GoogleMap/GoogleMap.js
@@ -3,25 +3,42 @@ import GoogleMapReact from 'google-map-react';
 import { googleApiKey } from '../../apiCalls/apiKeys/googleApiKey';
 import UserMarker from '../UserMarker/UserMarker';
 
- 
 class GoogleMap extends Component {
-  static defaultProps = {
-    center: {
-      lat: 39.750801,
-      lng: -104.996595
-    },
-    zoom: 16
-  };
+  constructor() {
+    super();
+
+    this.state = {
+      center: {
+        lat: 39.750801,
+        lng: -104.996595
+      },
+      zoom: 16
+    };
+  }
  
   render() {
     return (
       <div style={{ height: '400px', width: '100%' }}>
         <GoogleMapReact
           bootstrapURLKeys={{ key: googleApiKey }}
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
+          defaultCenter={this.state.center}
+          defaultZoom={this.state.zoom}
         >
-          <UserMarker/>
+          <UserMarker
+            key={0}
+          />
+          <UserMarker 
+            key={1}
+            lat={39.752816}
+            lng={-104.993984}
+            className="brothers"
+          />
+          <UserMarker 
+            key={2}
+            lat={39.750934}
+            lng={-104.999928}
+            className="brothers"
+          />
         </GoogleMapReact>
       </div>
     );
