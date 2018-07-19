0e9da1339b5e83ae5bd74da7001e52a41437468a
Ecksi <jimmy.dechant@gmail.com>
2018-07-19 17:22:20 -0600

    Add server.js boilerplate

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+const bodyParser = require('body-parser');
+
+app.set('port', process.env.PORT || 3000);
+app.locals.title = 'Happy Hour Hero';
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is running on ${app.get('port')}.`);
+});
+
+module.exports = app;
