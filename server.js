d45ebdb9a4260c883a848e0e696dab0ee57583a2
Ecksi <jimmy.dechant@gmail.com>
2018-07-19 17:23:12 -0600

    Add GET endpoint for all or one restaurant

--- server.js
+++ server.js
@@ -10,6 +10,24 @@ app.locals.title = 'Happy Hour Hero';
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
+app.get('/api/v1/restaurants', (request, response) => {
+  database('restaurants').select()
+    .then((restaurant) => {
+      response.status(200).json(restaurant);
+    })
+    .catch((error) => {
+      response.status(500).json({ error });
+    });
+});
+
+app.get('/api/v1/restaurants/:id', (request, response) => {
+  database('restaurants')
+    .select()
+    .where('id', request.params.id)
+    .then(restaurant => response.status(200).json(restaurant)[id])
+    .catch(error => response.status(500).json({ error }));
+});
+
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
 });
