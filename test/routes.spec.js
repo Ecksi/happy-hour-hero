1ba742b460c814f0b91db9f85f956c4d7408d69d
Ecksi <jimmy.dechant@gmail.com>
2018-07-19 21:51:55 -0600

    Add client routes tests

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -6,3 +6,26 @@ const config = require('../knexfile')[(process.env.NODE_ENV = 'test')];
 const knex = require('knex')(config);
 
 chai.use(chaiHttp);
+
+describe('CLIENT routes', () => {
+  it('should receive a response of html when we hit the root end point', done => {
+    chai
+      .request(server)
+      .get('/')
+      .end((error, response) => {
+        response.should.have.status(200);
+        response.should.be.html;
+        done();
+      });
+  });
+
+  it('should return a 404 for a route that does not exist', done => {
+    chai
+      .request(server)
+      .get('/capstone')
+      .end((error, response) => {
+        response.should.have.status(404);
+        done();
+      });
+  });
+});
