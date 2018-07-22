18bc33d274a5bf1638a432c529f931bcdcfdad52
Ecksi <jimmy.dechant@gmail.com>
2018-07-22 15:18:05 -0600

    Add snapshot test for ResultCard.js

--- src/components/ResultCard/ResultCard.test.js
+++ src/components/ResultCard/ResultCard.test.js
@@ -1,9 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { shallow } from 'enzyme';
+import ResultCard from './ResultCard';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('ResultCard', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<ResultCard />);
+  });
+
+  it('matches the snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
 });
