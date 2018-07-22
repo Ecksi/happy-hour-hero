040cceeb46d12ef31004c137f19a9da2549851b6
Ecksi <jimmy.dechant@gmail.com>
2018-07-22 15:29:43 -0600

    Add snapshot test for UserMarker.js

---
+++ src/components/UserMarker/UserMarker.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import UserMarker from './UserMarker';
+
+describe('UserMarker', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<UserMarker />);
+  });
+
+  it('matches the snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+});
