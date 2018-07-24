98f7ea43a15fd2545a222bb35c7ebc20288f8830
Ecksi <jimmy.dechant@gmail.com>
2018-07-24 13:44:48 -0600

    Remove unused constructor, and prop

--- src/components/RestaurantMarker/RestaurantMarker.js
+++ src/components/RestaurantMarker/RestaurantMarker.js
@@ -5,29 +5,23 @@ import PropTypes from 'prop-types';
 import restaurantMarker from './assets/restaurant-marker.png';
 
 export class RestaurantMarker extends Component {
-  constructor (props) {
-    super(props);
-  }
-
   handleClick = (event) => {
     const allCards = document.querySelectorAll('.resultCardContainer');
-
-    allCards.forEach(card => card.classList.remove('selectedCard'));
-
     const id = event.target.closest('section').getAttribute('id');
     const selectedSelector = `.resultCard${id}`;
     const selectedCard = document.querySelector(selectedSelector);
 
+    allCards.forEach(card => card.classList.remove('selectedCard'));
     selectedCard.classList.add('selectedCard');
     selectedCard.scrollIntoView({ behavior: 'smooth', block: 'center' });
   }
 
   render() {
-    const { name, id } = this.props;
+    const { id } = this.props;
 
     return (
       <section className={`restaurantMarker marker${id}`} id={id} onClick={(event) => this.handleClick(event)}>
-        <img src={ restaurantMarker } />
+        <img src={ restaurantMarker } alt='Restaurant Marker' />
       </section>
     );
   }
