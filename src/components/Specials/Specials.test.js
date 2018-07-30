a9825795edb3cd9aa45789a1be988ceca85b69c8
Ecksi <jimmy.dechant@gmail.com>
2018-07-29 20:23:38 -0600

    Add mapStateToProps test for Specials

--- src/components/Specials/Specials.test.js
+++ src/components/Specials/Specials.test.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { Specials } from './Specials';
+import { Specials, mapStateToProps } from './Specials';
 
 describe('Specials', () => {
   let wrapper;
+  let mockProps;
 
   beforeEach(() => {
+    mockProps = {
+      location: [],
+    };
     wrapper = shallow(<Specials />);
   });
 
   it('should match the snapshot', () => {
     expect(wrapper).toMatchSnapshot();
+  });
+
+  describe('mapStateToProps', () => {
+    it('can access locations from the store', () => {
+      const location = mockProps.locations;
+      const mockState = { location };
+      const mappedProps = mapStateToProps(mockState);
+
+      expect(mappedProps).toEqual(mockState);
+    });
   });
 });
