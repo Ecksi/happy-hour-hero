b93d1481157378fea8150c863f4982185000e787
Ecksi <jimmy.dechant@gmail.com>
2018-07-29 20:26:54 -0600

    Add mapStateToProps test for UserMarker

--- src/components/UserMarker/UserMarker.test.js
+++ src/components/UserMarker/UserMarker.test.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { UserMarker } from './UserMarker';
+import { UserMarker, mapStateToProps } from './UserMarker';
 
 describe('UserMarker', () => {
   let wrapper;
+  let mockProps;
 
   beforeEach(() => {
+    mockProps = {
+      location: []
+    };
     wrapper = shallow(<UserMarker />);
   });
 
   it('matches the snapshot', () => {
     expect(wrapper).toMatchSnapshot();
+  });
+
+  describe('mapStateToProps', () => {
+    it('can access locations from the store', () => {
+      const location = mockProps.locations;
+      const mockState = { location };
+      const mappedProps = mapStateToProps(mockState);
+
+      expect(mappedProps).toEqual(mockState);
+    });
   });
 });
