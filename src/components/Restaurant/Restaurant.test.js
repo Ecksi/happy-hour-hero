0f54d9ecb50df67663557258289ba445b1529131
Tyler McNamara <mcnamara14@gmail.com>
2018-07-30 11:23:31 -0600

    Add formatAddress tests

--- src/components/Restaurant/Restaurant.test.js
+++ src/components/Restaurant/Restaurant.test.js
@@ -12,4 +12,19 @@ describe('Restaurant', () => {
   it('matches the snapshot', () => {
     expect(wrapper).toMatchSnapshot();
   });
+
+  describe('formatAddress', () => {
+    beforeEach(() => {
+      wrapper = shallow(<Restaurant />);
+    });
+
+    it('should return the address in the correct format', async () => {
+      const mockAddress = 'Falling Rock Brewery Denver, CO'
+      const expected = 'falling+rock+brewery+denver+co';
+      const result = wrapper.instance().formatAddress(mockAddress);
+
+      expect(result).toEqual(expected);
+    });
+  });
+
 });
