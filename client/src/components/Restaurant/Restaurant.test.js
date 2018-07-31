425db024bd076dd1cb2955959295283d92659de3
Tyler McNamara <mcnamara14@gmail.com>
2018-07-31 16:35:13 -0600

    Add getFoodSpecials testing

--- client/src/components/Restaurant/Restaurant.test.js
+++ client/src/components/Restaurant/Restaurant.test.js
@@ -4,8 +4,57 @@ import { Restaurant } from './Restaurant';
 
 describe('Restaurant', () => {
   let wrapper;
+  let mockHappyHours;
+  let mockRestaurant;
+  let mockDrinkSpecials;
+  let mockFoodSpecials;
 
   beforeEach(() => {
+    mockHappyHours = [{
+      combined_times:"4:00PM-8:00PM",
+      created_at:"2018-07-30T13:59:04.931Z",
+      day:"Monday",
+      drink_specials_id:1,
+      end_time:"2000",
+      food_specials_id:1,
+      id:1,
+      restaurant_id:1,
+      start_time:"1600",
+      updated_at:"2018-07-30T13:59:04.931Z"
+    }];
+
+    mockRestaurant = {
+      id: 1, 
+      name: "Brothers Bar", 
+      address: "1920 Market St", 
+      phone: "(303) 297-2767", 
+      website: "http://www.brothersbar.com/denver-co/",
+      city: "Denver",
+      created_at: "2018-07-30T13:59:04.919Z",
+      latitude: "39.752816",
+      longitude:"-104.993984",
+      restaurant_image: "http://www.brothersbar.com/wp-content/uploads/2015/10/GALLERY-Stapleton.jpg",
+      state:"CO",
+      updated_at: "2018-07-30T13:59:04.919Z",
+      zip_code: 80202
+    };
+
+    mockDrinkSpecials = [{
+      best_deal: true,
+      created_at: "2018-07-30T13:59:04.910Z",
+      id: 1,
+      name: "2-for-1 drinks",
+      updated_at: "2018-07-30T13:59:04.910Z",
+    }];
+
+    mockFoodSpecials = [{
+      best_deal: true,
+      created_at: "2018-07-30T13:59:04.910Z",
+      id: 1,
+      name: "$3 tacos",
+      updated_at: "2018-07-30T13:59:04.910Z",
+    }];
+
     wrapper = shallow(<Restaurant />, { disableLifecycleMethods: true });
   });
 
@@ -44,22 +93,6 @@ describe('Restaurant', () => {
 
   describe('getRestaurantBySlug', () => {
     beforeEach(() => {
-      const mockRestaurant = {
-        id: 1, 
-        name: "Brothers Bar", 
-        address: "1920 Market St", 
-        phone: "(303) 297-2767", 
-        website: "http://www.brothersbar.com/denver-co/",
-        city: "Denver",
-        created_at: "2018-07-30T13:59:04.919Z",
-        latitude: "39.752816",
-        longitude:"-104.993984",
-        restaurant_image: "http://www.brothersbar.com/wp-content/uploads/2015/10/GALLERY-Stapleton.jpg",
-        state:"CO",
-        updated_at: "2018-07-30T13:59:04.919Z",
-        zip_code: 80202
-      };
-
       wrapper = shallow(<Restaurant />, { disableLifecycleMethods: true });
       window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
         json: () => Promise.resolve({
@@ -87,23 +120,9 @@ describe('Restaurant', () => {
   });
 
   describe('getTodaysHappyHours', () => {
-    let mockHappyHours;
     let wrapper;
 
     beforeEach(() => {
-      mockHappyHours = [{
-        combined_times:"4:00PM-8:00PM",
-        created_at:"2018-07-30T13:59:04.931Z",
-        day:"Monday",
-        drink_specials_id:1,
-        end_time:"2000",
-        food_specials_id:null,
-        id:1,
-        restaurant_id:1,
-        start_time:"1600",
-        updated_at:"2018-07-30T13:59:04.931Z"
-      }];
-
       wrapper = shallow(<Restaurant location={{name: 'Denver, CO'}}/>, { disableLifecycleMethods: true });
       window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
         json: () => Promise.resolve({
@@ -138,37 +157,12 @@ describe('Restaurant', () => {
   });
 
   describe('getDrinkSpecials', () => {
-    let mockDrinkSpecials;
-    let mockHappyHours;
     let wrapper;
 
     beforeEach(() => {
-      mockHappyHours = [{
-        combined_times:"4:00PM-8:00PM",
-        created_at:"2018-07-30T13:59:04.931Z",
-        day:"Monday",
-        drink_specials_id:1,
-        end_time:"2000",
-        food_specials_id:null,
-        id:1,
-        restaurant_id:1,
-        start_time:"1600",
-        updated_at:"2018-07-30T13:59:04.931Z"
-      }];
-
-      mockDrinkSpecials = [{
-        best_deal: true,
-        created_at: "2018-07-30T13:59:04.910Z",
-        id: 1,
-        name: "2-for-1 drinks",
-        updated_at: "2018-07-30T13:59:04.910Z",
-      }];
-
       wrapper = shallow(<Restaurant location={{name: 'Denver, CO'}}/>, { disableLifecycleMethods: true });
       window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
-        json: () => Promise.resolve({
-          drinkSpecial: mockDrinkSpecials 
-        })
+        json: () => Promise.resolve(mockDrinkSpecials)
       }));
 
       wrapper.instance().getFoodSpecials = jest.fn();
@@ -177,7 +171,6 @@ describe('Restaurant', () => {
       });
     });
 
-    // One of the two tests below work but not both for some reason
     it('should fetch url with the correct arguments', async () => {
       const expected = 'http://localhost:3000/api/v1/drink_specials/1';
       await wrapper.instance().getDrinkSpecials();
@@ -185,19 +178,51 @@ describe('Restaurant', () => {
       expect(window.fetch).toHaveBeenCalledWith(expected);
     });
 
-    it.skip('should set state to restaurants happy hour times', async () => {
-      const expected = { "mockHappyHours": mockHappyHours};
-
+    it('should set state to restaurants happy hour times', async () => {
       await wrapper.instance().getDrinkSpecials();
 
-      expect(wrapper.state('drinkSpecials')).toEqual(expected);
+      expect(wrapper.state('drinkSpecials')).toEqual(mockDrinkSpecials);
     });
 
-    it.skip('should set state to restaurants happy hour times', async () => {
-      const expected = { "mockHappyHours": mockHappyHours};
+    it('should set state to restaurants happy hour times', async () => {
       await wrapper.instance().getDrinkSpecials();
 
-      expect(wrapper.state('drinkSpecials')).toEqual(expected);
+      expect(wrapper.state('drinkSpecials')).toEqual(mockDrinkSpecials);
+    });
+  });
+
+  describe('getFoodSpecials', () => {
+    let wrapper;
+
+    beforeEach(() => {
+      wrapper = shallow(<Restaurant location={{name: 'Denver, CO'}}/>, { disableLifecycleMethods: true });
+      window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+        json: () => Promise.resolve(mockFoodSpecials)
+      }));
+
+      wrapper.instance().cleanHappyHourTimes = jest.fn();
+      wrapper.setState({
+        todaysHappyHours: mockHappyHours
+      });
+    });
+
+    it('should fetch url with the correct arguments', async () => {
+      const expected = 'http://localhost:3000/api/v1/food_specials/1';
+      await wrapper.instance().getFoodSpecials();
+
+      expect(window.fetch).toHaveBeenCalledWith(expected);
+    });
+
+    it('should set state to restaurants happy hour times', async () => {
+      await wrapper.instance().getFoodSpecials();
+
+      expect(wrapper.state('foodSpecials')).toEqual(mockFoodSpecials);
+    });
+
+    it('should set state to restaurants happy hour times', async () => {
+      await wrapper.instance().getFoodSpecials();
+
+      expect(wrapper.state('foodSpecials')).toEqual(mockFoodSpecials);
     });
   });
 });
