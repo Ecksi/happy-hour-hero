ec18e7047b44d6b688820ea77a16e741bc9e27d2
Tyler McNamara <mcnamara14@gmail.com>
2018-07-31 16:11:11 -0600

    Add styling edits to RestaurantInto, RestaurantCard, and RestaurantHappyHours

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,6 +5,7 @@ import HappyHours from '../HappyHours/HappyHours';
 import Restaurant from '../Restaurant/Restaurant';
 import PropTypes from 'prop-types';
 import './App.css';
+import image from './images/happy-hour-home-bg.jpg';
 
 class App extends Component {
   handleSubmit = () => {
@@ -12,8 +13,10 @@ class App extends Component {
   }
 
   render() {
+    const backgroundImage = {backgroundImage: "url(" + image + ")"};
+    
     return (
-      <div className="App">
+      <div className="App"  style={backgroundImage}>
         <Route exact path='/' component={Home} />
         <Route exact path='/happy-hours' component={HappyHours} />
         <Route exact path='/restaurant' component={Restaurant} />
