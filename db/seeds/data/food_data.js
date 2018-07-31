4269df8cee8c727124c53c81cb605b3bcbcf3dc0
Ecksi <jimmy.dechant@gmail.com>
2018-07-31 14:11:36 -0600

    Move seed data into seed file

--- db/seeds/data/food_data.js
+++ db/seeds/data/food_data.js
@@ -1,13 +1,13 @@
 module.exports = [
   { name: '25¢ Wings', best_deal: true },
-  { name: '$2.00 Cheeseburger', best_deal: true },
+  { name: '$2 Cheeseburger', best_deal: true },
   { name: '2 Sushi Rolls for $8.30', best_deal: true },
   { name: 'Almost half off starters', best_deal: false },
   { name: 'Happy Hour Nigiri', best_deal: false },
-  { name: '$1.00 Oysters', best_deal: true },
-  { name: '$3.00 Select Appetizers', best_deal: false },
-  { name: '$4.00 Select Appetizers', best_deal: false },
-  { name: '$5.00 Select Appetizers', best_deal: false },
-  { name: '$1.00 Off All Appetizers', best_deal: true },
+  { name: '$1 Oysters', best_deal: true },
+  { name: '$3 Select Appetizers', best_deal: false },
+  { name: '$4 Select Appetizers', best_deal: false },
+  { name: '$5 Select Appetizers', best_deal: false },
+  { name: '$1 Off All Appetizers', best_deal: true },
   { name: '$10 Burger & Beer', best_deal: true }
 ]
