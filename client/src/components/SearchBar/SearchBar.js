ae4fcdcc2a10c8b1a7e85b5550f1994076d079d0
Ecksi <jimmy.dechant@gmail.com>
2018-08-01 08:16:46 -0600

    Fix for heroku deployment

--- client/src/components/SearchBar/SearchBar.js
+++ client/src/components/SearchBar/SearchBar.js
@@ -11,7 +11,7 @@ import geolib from 'geolib';
 import { storeLocation, storeRestaurants, storeFilteredRestaurants, storeHappyHours, storeDrinkSpecials, storeFoodSpecials, storeRestaurantId } from '../../actions';
 import PropTypes from 'prop-types';
 import './SearchBar.css';
-require('dotenv').config();
+// require('dotenv').config();
 let googleApiKey = process.env.REACT_APP_GOOGLE_API_KEY;
 
 export class SearchBar extends Component {
